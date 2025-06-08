8c0b2c1d99d7069adfbc283de591219c31fc5022
suryakant0111 <suryakantvkhevji@gmail.com>
2025-06-08 14:47:33 +0530

    Utils code discussion

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,20 @@
+const asyncHandler1 = (fn)=>{
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        console.error("Error in asyncHandler:", error);
+        res.status(500).json({ message: "Internal Server Error", success: false });
+        
+    }
+}
+
+
+export{asyncHandler, asyncHandler1};
